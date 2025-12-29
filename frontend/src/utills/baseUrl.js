50725df6bb1e06fc758cb0b3fd96c889919c799b
hanzala bawany <hanzalabawany0@gmail.com>
2025-12-29 15:43:12 +0500

    changes

--- frontend/src/utills/baseUrl.js
+++ frontend/src/utills/baseUrl.js
@@ -1,6 +1,7 @@
 export const base_URL = "http://localhost:3000";  
 // export const base_URL = "http://192.168.100.13:3000";
 // export const base_URL = "http://192.168.100.57:3000";
+// export const base_URL = "http://192.168.100.14:3000";
 
 
 
